53cd43b78e0b4856909940fc37ee205787fe025b
yurika tsuji <sakurasou.tystu@gmail.com>
2025-02-20 20:56:43 +0900

    マイグレーションファイルの追加

---
+++ src/database/migrations/2025_02_20_065518_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('image');
+            $table->integer('category_id');
+            $table->integer('quality_id');
+            $table->string('name');
+            $table->string('brand');
+            $table->text('description');
+            $table->integer('price');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
